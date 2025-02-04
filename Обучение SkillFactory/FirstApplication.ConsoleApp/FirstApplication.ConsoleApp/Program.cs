985874f6abfb14049641711488f87bf66572b5ae
Holdonz <nikita.kocarevv@gmail.com>
2025-02-04 09:51:20 +0300

    load files

---
+++ Обучение SkillFactory/FirstApplication.ConsoleApp/FirstApplication.ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApplication.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как Вас зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Здравствуйте, {name}");
+            Console.WriteLine("Сколько Вам лет?");
+            string age = Console.ReadLine();
+            Greet(name, age);
+            Console.WriteLine("Рад был познакомиться!");
+        }
+
+        static void Greet(string name, string age)
+        {
+            Console.WriteLine("Здравствуйте, {0}", name);
+            Console.WriteLine("Мне сообщили, что Вам {0}", age);
+        }
+    }
+}
