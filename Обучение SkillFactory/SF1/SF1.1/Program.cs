d1e1d2204f4c5376bb12695038be2c1207a51cae
Holdonz <nikita.kocarevv@gmail.com>
2025-02-10 18:29:29 +0300

    Добавил дополнение к первому проекту

---
+++ Обучение SkillFactory/SF1/SF1.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string myName = "Никита";
+            byte age = 18;
+            bool pet = true;
+            double sizeFoot = 41.5;
+
+            Console.WriteLine("Меня зовут " + myName);
+            Console.WriteLine("Мне " + age);
+            Console.WriteLine("У меня есть собака, его зовут Балу " + pet);
+            Console.WriteLine("Мой размер ноги " + sizeFoot);
+        }
+    }
+}
