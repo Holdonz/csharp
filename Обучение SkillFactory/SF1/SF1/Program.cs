80da685684613e86c148a92e88e0699fc802e2ad
Holdonz <nikita.kocarevv@gmail.com>
2025-02-10 11:53:49 +0300

    Создал первый проект для обучения SkillFactory

---
+++ Обучение SkillFactory/SF1/SF1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SF1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string MyName = "Nikita";
+            Console.WriteLine(MyName);
+
+            Console.WriteLine("\t Привет Мир");
+            Console.WriteLine("\t Мне 27 лет");
+            Console.WriteLine("\t My name is \n {0}", MyName);
+            Console.WriteLine("\u0040");
+            Console.WriteLine("\x23");
+
+            Console.ReadKey();
+        }
+    }
+}
