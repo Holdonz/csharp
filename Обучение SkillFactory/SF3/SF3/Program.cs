64415781ada2cf02361fff82bacdcf9bee587de8
Holdonz <nikita.kocarevv@gmail.com>
2025-02-12 20:46:06 +0300

    Сделал 3 задние

---
+++ Обучение SkillFactory/SF3/SF3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Напишите свой любимый цвет с маленькой буквы: ");
+
+            switch (Console.ReadLine())
+            {
+                case "красный":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Твой цвет - красный!");
+                break;
+
+                case "зеленый":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Твой цвет - зеленый!");
+                break;
+
+                case "голубой":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Твой цвет - голубой!");
+                    break;
+
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Твой цвет - желытый!");
+                break;
+
+            }
+        }
+    }
+}
