2aa9a09f78d15277af7be885549189ca0679e701
Holdonz <nikita.kocarevv@gmail.com>
2025-02-13 11:48:32 +0300

    Сделал 2 задание из ютуб курса

---
+++ Задания из ютуба/Проверка числа на четность/Проверка числа на четность/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Проверка_числа_на_четность
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число для проверки на четность: ");
+            double  value = double.Parse(Console.ReadLine());
+            
+            //Если введеное число делится без остатка на 2 (True), то выполнится первое условие, иначе второе.
+            if (value % 2 == 0)
+                Console.WriteLine("Число четное");
+            else
+                Console.WriteLine("Число нечетное");
+        }
+    }
+}
