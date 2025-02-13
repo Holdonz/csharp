5a4133e74679836f842a351901e6cc855d20e034
Holdonz <nikita.kocarevv@gmail.com>
2025-02-13 14:01:16 +0300

    Написал простой калькулятор двумя способами

---
+++ Задания из ютуба/Калькулятор с использованием Switch/Калькулятор с использованием Switch/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Калькулятор_с_использованием_Switch
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.Write("Введите арифметическую операцию: ");
+            string operation = Console.ReadLine();
+
+            Console.Write("Введите первый операнд: ");
+            double firstOperand = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите второй операнд: ");
+            double secondOperand = double.Parse(Console.ReadLine());
+
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine($"Результат:\n{firstOperand} + {secondOperand} = {firstOperand + secondOperand}");
+                    break;
+
+                case "-":
+                    Console.WriteLine($"Результат:\n{firstOperand} - {secondOperand} = {firstOperand - secondOperand}");
+                    break;
+
+                case "*":
+                    Console.WriteLine($"Результат:\n{firstOperand} * {secondOperand} = {firstOperand * secondOperand}");
+                    break;
+
+                case "/":
+                    Console.WriteLine($"Результат:\n{firstOperand} / {secondOperand} = {firstOperand / secondOperand}");
+                    break;
+
+                default:
+                    Console.WriteLine("Вы ввели неизвестный оператор!");
+                    break;
+            }
+        }
+    }
+}
---
+++ Задания из ютуба/Калькулятор с использованием Switch/Калькулятор с использованием if else/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Калькулятор с использованием if else")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Калькулятор с использованием if else")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("db1b91bc-d214-45de-8bd2-eb8e942a2fc8")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
