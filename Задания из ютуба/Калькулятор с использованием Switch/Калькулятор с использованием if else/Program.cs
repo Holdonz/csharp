9bef9f501d6220ce11b5c4f32820181edc0c1df5
Holdonz <nikita.kocarevv@gmail.com>
2025-02-13 14:01:16 +0300

    Написал простой калькулятор двумя способами

---
+++ Задания из ютуба/Калькулятор с использованием Switch/Калькулятор с использованием if else/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Калькулятор_с_использованием_if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите арифметическую операцию: ");
+            string operation = Console.ReadLine();
+
+            Console.Write("Введите первый операнд: ");
+            double firstOperand = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите второй операнд: ");
+            double secondOperand = double.Parse(Console.ReadLine());
+
+            if (operation == "+")
+                Console.WriteLine($"Результат:\n{firstOperand} + {secondOperand} = {firstOperand + secondOperand}");
+
+            else if (operation == "-")
+                Console.WriteLine($"Результат:\n{firstOperand} - {secondOperand} = {firstOperand - secondOperand}");
+
+            else if (operation == "*")
+                Console.WriteLine($"Результат:\n{firstOperand} * {secondOperand} = {firstOperand * secondOperand}");
+
+            else if (operation == "/")
+                Console.WriteLine($"Результат:\n{firstOperand} / {secondOperand} = {firstOperand / secondOperand}");
+
+            else
+                Console.WriteLine("Вы ввели неизвестный оператор!");
+        }
+    }
+}
