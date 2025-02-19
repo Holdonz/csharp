26ce87089444af1a25bed9a5c5fc373795927cb1
Holdonz <nikita.kocarevv@gmail.com>
2025-02-19 15:11:35 +0300

    Сделал проект, не зная как он работает)

---
+++ МДК 01.01/ConsoleApp1/ConsoleApp1/Geometry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Geometry
+    {
+        //Пока ничего не понимаю, но очень хочется разобраться
+        public double RectangleArea(double h, double r, double p)
+        {
+            return p * (r*r) * h;
+        }
+    }
+}
---
+++ МДК 01.01/ConsoleApp1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ConsoleApp1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    //Здесь так тем более я ничего не понимаю, для чего это как это и тд.
+    //Но рано или поздно я пойму, что здесь да как.
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double p = 3.14;
+            double r = 3;
+            double h = 4;
+            double expected = 113.04;
+            Geometry g = new Geometry();
+            double actual = g.RectangleArea(p, r, h);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
