38669321b91e31f3ffc6a0019c2e72d735efd920
Holdonz <nikita.kocarevv@gmail.com>
2025-03-11 15:03:50 +0300

    Пытался повторить метод Resize | не получилось(

---
+++ Задания из ютуба/Методы/Метод (Resize)/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Метод__Resize_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество элементов в массиве: ");
+            int[] myArray = new int[int.Parse(Console.ReadLine())];
+
+            Console.WriteLine("Хотите заполнить массив данными?: ");
+            string answer = Console.ReadLine();
+            if (answer == "Да" || answer == "да" || answer == "ДА")
+            {
+                for (int i = 0; i < myArray.Length; i++)
+                {
+                    Console.WriteLine($"Введите элемент массива под индексом {i}: ");
+                    myArray[i] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("На сколько элементов вы хотите увеличить или уменьшить массив?");
+            int size = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Ваш массив:");
+            Console.WriteLine();
+            Array.Resize(ref myArray, 5);
+            Resize(ref myArray, size);
+        }
+        
+        //данный метод не работает как надо. Он добавляет элементы и при этом не помещает старые элементы в новый массив
+        static void Resize(ref int[] array, int newSize)
+        {
+            int[] array2 = new int[array.Length + newSize];
+            array = array2;
+            if (0 > array.Length + newSize)
+            {
+                Console.WriteLine("Количество элементов не может быть отрицательным");
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{i}: {array[i]}");
+            }                       
+        }
+    }
+}
---
+++ Задания из ютуба/Методы/Метод (Resize)/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Метод (Resize)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Метод (Resize)")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("63662177-8801-442f-9ad9-bd5d702edbef")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
